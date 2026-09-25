988f2d048037a60f4a00d2d04fcc54f34ae9c25e
Estella Garcia <estellagarciasilva@gmail.com>
2026-09-25 20:31:17 -0300

    Adicionando informações do celeiro

--- src/main/java/br/com/joaocarloslima/Celeiro.java
+++ src/main/java/br/com/joaocarloslima/Celeiro.java
@@ -1,5 +1,39 @@
 package br.com.joaocarloslima;
 
 public class Celeiro {
-    
+    private int capacidade;
+    private int qtdeBatatas;
+    private int qtdeCenouras;
+    private int qtdeMorangos;
+
+    public Celeiro (int capacidade){
+        this.capacidade = capacidade;
+        this.qtdeBatatas = 1;
+        this.qtdeCenouras = 1;
+        this.qtdeMorangos = 1;   
+    }
+
+    public void armazenarBatata(){
+        if (celeiroCheio()){
+            throw new RuntimeException("O celeiro está cheio!");
+        }
+        this.qtdeBatatas += 2; // cada vez que colhe, adiciona duas unidades ao celeiro
+    }
+
+    public void armazenarCenoura(){
+        if(celeiroCheio()){
+            throw new RuntimeException("O celeiro está cheio!");
+        }
+        this.qtdeCenouras += 2;
+    }
+
+    public void armazenarMorango(){
+        if(celeiroCheio()){
+            throw new RuntimeException("O celeiro está cheio!");
+        }
+        this.qtdeMorangos +=2;
+    }
+
+
+
 }
