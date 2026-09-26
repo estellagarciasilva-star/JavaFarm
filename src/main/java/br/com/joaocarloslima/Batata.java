f2f576215ddb980d7e848a4208e0a9e037396062
Estella Garcia <estellagarciasilva@gmail.com>
2026-09-25 21:15:41 -0300

    classe Celeiro e Batata

--- src/main/java/br/com/joaocarloslima/Batata.java
+++ src/main/java/br/com/joaocarloslima/Batata.java
@@ -1,5 +1,42 @@
 package br.com.joaocarloslima;
 
 public class Batata {
-    
-}
+    private int tamanho;
+    private int tempoDeVida;
+    private int tempoDeCrescimento;
+
+    public Batata (int tamanho){
+        this.tamanho = 1;
+        this.tempoDeVida = 1;
+        this.tempoDeCrescimento = 3;
+    }
+
+    public void crescer(){  //incrementa o tempo de vida da planta e verifica se o tamanho deve ser incrementado dependendo do tempo de crescimento
+        this.tempoDeVida++;
+        if (this.tempoDeVida % this.tempoDeCrescimento == 0 && this.tamanho < 4){
+            this.tamanho++;
+        }
+    }
+
+        public boolean podeColher(){ //retorna verdadeiro se o alcançou o tamanho máximo
+            return this.tamanho >= 4;
+
+        }
+
+        public String getImagem(){ // imagem da batata em crescimento
+            return "images/batata" + this.tamanho + ".png";
+        }
+
+        public int getTamanho() {
+            return tamanho;
+        }
+
+        public int getTempoDeVida() {
+            return tempoDeVida;
+        }
+
+        public int getTempoDeCrescimento() {
+            return tempoDeCrescimento;
+        }
+    }
+
--- src/main/java/br/com/joaocarloslima/Celeiro.java
+++ src/main/java/br/com/joaocarloslima/Celeiro.java
@@ -34,6 +34,62 @@ public class Celeiro {
         this.qtdeMorangos +=2;
     }
 
+    public void consumirBatata(){ // remove uma unidade do produto, caso não tenha aparece a exceção
+        if(this.qtdeBatatas <=0){
+            throw new RuntimeException("Não tem batatas sufiecientes no celeiro para consumir!");
+        }
+        this.qtdeBatatas--;
+    }
+
+    public void consumirCenoura(){
+        if(this.qtdeCenouras <=0){
+            throw new RuntimeException("Não tem cenouras suficientes no celeiro para consumir!");
+        }
+        this.qtdeCenouras--;
+    }
+
+    public void consumirMorango(){
+        if(this.qtdeMorangos <=0){
+            throw new RuntimeException("Não tem morangos suficientes no celeiro para consumir!");
+        }
+    }
+
+    public int getEspacoDisponivel(){ // quantidade disponivel no celeiro
+        int totalProdutos = this.qtdeBatatas + this.qtdeCenouras + this.qtdeMorangos;
+        return this.capacidade - totalProdutos;
+    }
+
+    public double getOcupacao(){ // numero decimal com a porcentagem da ocupação no celeiro
+        int totalProdutos = this.qtdeBatatas + this.qtdeCenouras + this.qtdeMorangos;
+        return ((double) totalProdutos / this.capacidade) * 100.0;
+    }
+
+    public boolean celeiroCheio(){ // retorna verdadeiro caso o celeiro esteja completamente ocupado
+        return getEspacoDisponivel() <= 0;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public int getQtdeBatatas() {
+        return qtdeBatatas;
+    }
+
+    public int getQtdeCenouras() {
+        return qtdeCenouras;
+    }
+
+    public int getQtdeMorangos() {
+        return qtdeMorangos;
+    }
+
+    
+
+
+
+
+
 
 
 }
